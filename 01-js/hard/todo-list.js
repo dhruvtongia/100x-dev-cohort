534e8e52653fd506086f36f44e39f9328e206bf4
dhruvtongia <dhruvtongia2000@gmail.com>
2023-12-12 00:33:34 +0530

    solved to-do list assignment

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,34 @@
 */
 
 class Todo {
-
+  constructor() {
+    this.todoList = new Array();
+  }
+  add(todo) {
+    this.todoList.push(todo);
+  }
+  remove(indexOfTodo) {
+    if(indexOfTodo>-1 && indexOfTodo<this.todoList.length ) {
+      this.todoList.splice(indexOfTodo,1);
+    }
+  }
+  update(index, updatedTodo) {
+    if(index>-1 && index<this.todoList.length ) {
+      this.todoList[index] = updatedTodo;
+    }
+  }
+  getAll() {
+    return this.todoList;
+  }
+  clear() {
+    this.todoList = [];
+  }
+  get(index) {
+    if(index>-1 && index<this.todoList.length ) {
+      return this.todoList[index] ;
+    }
+    return null;
+  }
 }
 
 module.exports = Todo;
