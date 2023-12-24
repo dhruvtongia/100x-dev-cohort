824d01cebb506d5f2e4a289677b2f9a08a7da266
dhruvtongia <dhruvtongia2000@gmail.com>
2023-12-25 01:36:04 +0530

    file assignment solution

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -17,5 +17,27 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.get('/files', (req, res) => {
+  fs.readdir(path.join(__dirname, './files/'), (err, data) => {
+    if(err)
+     return res.status(500).json({msg: "Failed to retrieve files."});
+  
+     return res.status(200).json(data);
+  })
+})
 
+app.get('/file/:filename', (req,res) => {
+  const filePath = path.join(__dirname,"./files",req.params.filename);
+
+  fs.readFile(filePath,'utf8', (err, data) => {
+    if(err)
+    return res.status(404).send('File not found');
+ 
+    return res.status(200).json(data);
+  })
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('Route not found');
+});
 module.exports = app;
